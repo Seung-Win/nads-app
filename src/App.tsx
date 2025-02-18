9bf0bcb08614bc69a0b1126021696c44dda46d45
Andrei Rouiz Pascual <andrei@192.168.1.7>
2025-02-18 22:45:40 +0800

    Add Feat: Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Background from "./components/Background";
 import Heading from "./components/Heading"
-import Home from "./components/Home"
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Contact from "./pages/contact/Contact"
 
 function App() {
   return (
-    <div>
+    <Router>
       <Background />
       <Heading />
-      <Home />
-    </div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Heading />} />
+        <Route path="/contact" element={<Background />} />
+      </Routes> 
+    </Router>
   );
 }
 
