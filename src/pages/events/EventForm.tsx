375b64e42581ab461eda8e700213d8a8348142f8
anasel <anaselace14@gmail.com>
2025-02-26 00:59:46 +0800

    university dropdown

--- src/pages/events/EventForm.tsx
+++ src/pages/events/EventForm.tsx
@@ -71,25 +71,65 @@ function EventForm() {
             <label>Birth Date</label>
             <input type="date" className="form-control" id="bdateInput" />
           </div>
-        </div>
+        </div>    
         <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          id="emailInput"
-          aria-describedby="emailHelp"
-          placeholder="e.g. juandcruz@gmail.com"
-          required
-        />
+          <input
+            type="email"
+            className="form-control"
+            id="emailInput"
+            aria-describedby="emailHelp"
+            placeholder="e.g. juandcruz@gmail.com"
+            required
+          />  
         <label>University (do not abbreviate)</label>
-        <input
-          type="text"
-          className="form-control"
-          id="emailInput"
-          aria-describedby="emailHelp"
-          placeholder="e.g. Polytechnic University of the Philippines - Mabini Campus"
-          required
-        />
+          <input 
+            className="form-control"
+            id="universityInput" 
+            list="universities"
+            placeholder="e.g. Polytechnic University of the Philippines - Manila" 
+            required 
+          />
+          <datalist id="universities">
+            <option value="Adamson University"></option>
+            <option value="Ateneo de Manila University"></option>
+            <option value="Centro Escolar University - Manila"></option>
+            <option value="Colegio de San Juan de Letran - Manila"></option>
+            <option value="De La Salle University"></option>
+            <option value="Eulogio 'Amang' Rodriguez Institute of Science and Technology - Manila"></option>
+            <option value="Far Eastern University - Dr. Nicanor Reyes Medical Foundation"></option>
+            <option value="Far Eastern University - Institute of Technology"></option>
+            <option value="Far Eastern University - Manila"></option>
+            <option value="Lyceum of the Philippines University - Manila"></option>
+            <option value="Manila Central University"></option>
+            <option value="Mapúa University - Intramuros"></option>
+            <option value="Mapúa University - Makati"></option>
+            <option value="Marikina Polytechnic College"></option>
+            <option value="National University - Manila"></option>
+            <option value="New Era University - Main"></option>
+            <option value="Our Lady of Fatima University - QC"></option>
+            <option value="Our Lady of Fatima University - Valenzuela"></option>
+            <option value="Pamansatan ng Lungsod ng Maynila"></option>
+            <option value="PATTS College of Aeronautics"></option>
+            <option value="Philippine Normal University"></option>
+            <option value="Philippine State College of Aeronautics - Villamor"></option>
+            <option value="Polytechnic University of the Philippines - Manila"></option>
+            <option value="Polytechnic University of the Philippines - San Juan"></option>
+            <option value="Polytechnic University of the Philippines - Taguig"></option>
+            <option value="Rizal Technological University - Boni"></option>
+            <option value="Rizal Technological University - Pasig"></option>
+            <option value="Southville International School and Colleges"></option>
+            <option value="Technological Institute of the Philippines - Manila"></option>
+            <option value="Technological Institute of the Philippines - QC"></option>
+            <option value="Technological University of the Philippines - Manila"></option>
+            <option value="Technological University of the Philippines - Taguig"></option>
+            <option value="Trinity University of Asia"></option>
+            <option value="University of Santo Tomas"></option>
+            <option value="University of the East - Caloocan"></option>
+            <option value="University of the East - Manila"></option>
+            <option value="University of the Philippines - Diliman"></option>
+            <option value="University of the Philippines - Manila"></option>
+            <option value="World Citi Colleges Aeronautical & Technological College - North Manila"></option>
+          </datalist>
         <div className="row">
           <div className="col">
             <label>Program (do not abbreviate)</label>
